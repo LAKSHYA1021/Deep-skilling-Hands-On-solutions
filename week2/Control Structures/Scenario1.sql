905f0104322c9b7dd95fa21d372ee525be3fffb6
Lakshya Saxena <lakshyasaxena1021@gmail.com>
2025-06-29 11:38:13 +0530

    Update Scenario1.sql

--- week2/Control Structures/Scenario1.sql
+++ week2/Control Structures/Scenario1.sql
@@ -14,6 +14,15 @@ CREATE TABLE Loans (
     EndDate DATE,
     FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
 );
+INSERT INTO Customers (CustomerID, Name, DOB, Balance, LastModified)
+VALUES (1, 'John Doe', TO_DATE('1985-05-15', 'YYYY-MM-DD'), 1000, SYSDATE);
+
+INSERT INTO Customers (CustomerID, Name, DOB, Balance, LastModified)
+VALUES (2, 'Jane Smith', TO_DATE('1990-07-20', 'YYYY-MM-DD'), 1500, SYSDATE);
+
+INSERT INTO Loans (LoanID, CustomerID, LoanAmount, InterestRate, StartDate, EndDate)
+VALUES (1, 1, 5000, 5, SYSDATE, ADD_MONTHS(SYSDATE, 60));
+
 
 begin 
 for res in (
