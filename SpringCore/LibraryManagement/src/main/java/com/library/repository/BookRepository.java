668e2c18a3032fdb70b358f9fec35ef6f026f7ba
Lakshya Saxena <lakshyasaxena1021@gmail.com>
2025-07-03 12:16:06 +0530

    Update BookRepository.java

--- SpringCore/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
+++ SpringCore/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -1,5 +1,8 @@
 package com.library.repository;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class BookRepository {
     public void getBookList() {
         System.out.println("Fetching list of books from the repository...");
