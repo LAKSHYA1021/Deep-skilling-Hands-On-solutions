fc3d11de31a5f258321f7cf828de47ba30555b5e
Lakshya Saxena <lakshyasaxena1021@gmail.com>
2025-07-13 14:51:52 +0530

    Create SpringLearnApplication.java

---
+++ week 4/2/spring-learn-HelloWorld/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -0,0 +1,28 @@
+package com.cognizant.springlearn;
+
+import com.cognizant.springlearn.Country;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLearnApplication.class, args);
+        LOGGER.info("START: main()");
+        displayCountry();
+        LOGGER.info("END: main()");
+    }
+
+    public static void displayCountry() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        Country country = context.getBean("country", Country.class);
+        LOGGER.debug("Country: {}", country);
+    }
+}
