97f2208e2a9ceee9546fe488bfa9ae7a9cbfeae2
Lakshya Saxena <lakshyasaxena1021@gmail.com>
2025-08-03 08:01:54 +0530

    Create User.js

---
+++ week 7/12/ticketbookingapp/src/User.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function User() {
+  return (
+    <div style={{ color: 'green' }}>
+      <h2>Welcome Back, User! ✅</h2>
+      <p>You are logged in. You can now book your flight tickets:</p>
+      <button>Book Now</button>
+    </div>
+  );
+}
+
+export default User;
